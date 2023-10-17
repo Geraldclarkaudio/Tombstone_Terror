0d3550d94ae3ea8330444522b4c3de1a71a3d8c8
Gerald Clark <geraldclarkaudio@gmail.com>
2023-10-16 22:06:31 -0500

    Set up Unity Project

---
+++ Assets/GameDevHQ/Filebase/Projects/Environments/Level_Design/Goblins_Pumpkins/Scripts/Animation_Offset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Offset : MonoBehaviour
+{
+    private Animator _anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _anim.SetFloat("Offset", Random.Range(0.0f, 1.0f));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
